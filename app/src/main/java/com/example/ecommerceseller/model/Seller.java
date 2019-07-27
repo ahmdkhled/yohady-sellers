b32daec61e62d3d7bf3aad340478103f80239b74
ibrahim karim othman <ibrahimothmanre@gmail.com>
2019-07-27 18:54:30 +0200

    Create seller model class

---
+++ app/src/main/java/com/example/ecommerceseller/model/Seller.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceseller.model;
+
+public class Seller {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String state;
+    private String city;
+    private String address;
+    private String mobile;
+    private String marketName;
+}
